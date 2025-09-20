67e016fb4f39374e02877f2ce33f88ddf81a94dd
antifloodbot <36970838+antifloodbot@users.noreply.github.com>
2025-09-20 11:16:05 +0100

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+
+    changeUsername(name) {
+        this.username = name;
+    },
+
+    updatePlayTime(num) {
+        this.playTime += num;
+    }
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
